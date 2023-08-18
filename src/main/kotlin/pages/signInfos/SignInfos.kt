f1a5be23fb81a7689e5271a665d1f5bb3c875e48
jixiaoyong <jixiaoyong1995@gmail.com>
2023-08-18 21:15:30 +0800

    Initial commit

---
+++ src/main/kotlin/pages/signInfos/SignInfos.kt
@@ -0,0 +1,9 @@
+package pages.signInfos
+
+/**
+ * @author : jixiaoyong
+ * @description ：TODO
+ *
+ * @email : jixiaoyong1995@gmail.com
+ * @date : 2023/8/18
+ */
---
+++ src/main/kotlin/pages/signapp/SignApp.kt
@@ -0,0 +1,10 @@
+package pages.signapp
+
+/**
+ * @author : jixiaoyong
+ * @description ：TODO
+ *
+ * @email : jixiaoyong1995@gmail.com
+ * @date : 2023/8/18
+ */class SignApp {
+}
