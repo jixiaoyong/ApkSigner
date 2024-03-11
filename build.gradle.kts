42fef3e00fffd944e5df87e7fdae7989a6be89ff
JI,XIAOYONG <jixiaoyong1995@gmail.com>
2024-03-11 18:03:11 +0800

    build: 更新当前版本为1.1.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,7 +35,7 @@ compose.desktop {
         nativeDistributions {
             targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
             packageName = "ApkSigner"
-            packageVersion = "1.1.2"
+            packageVersion = "1.1.3"
 
             buildTypes.release {
                 proguard {
