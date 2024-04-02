32b0b18e1e8db3cfe4f6260bbc3a56047c5a0e6e
JI,XIAOYONG <jixiaoyong1995@gmail.com>
2024-04-02 17:33:54 +0800

    refactor: 更新依赖方式，统一使用libs.versions.xml管理

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,10 @@
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 
+@Suppress("DSL_SCOPE_VIOLATION")
 plugins {
-    kotlin("jvm")
-    id("org.jetbrains.compose")
-    id("com.google.devtools.ksp")
+    alias(libs.plugins.kotlin)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.compose)
 }
 
 group = "io.github.jixiaoyong"
@@ -18,27 +19,22 @@ repositories {
 
 ksp {
     arg("lyricist.generateStringsProperty", "true")
-
 }
 
-val multiplatformSettings = "1.1.1"
-val lyricist = "1.6.2-1.8.20"
 dependencies {
-    // Note, if you develop a library, you should use compose.desktop.common.
-    // compose.desktop.currentOs should be used in launcher-sourceSet
-    // (in a separate module for demo project and in testMain).
-    // With compose.desktop.common you will also lose @Preview functionality
     implementation(compose.desktop.currentOs)
-    implementation("com.russhwolf:multiplatform-settings-no-arg:$multiplatformSettings")
-    implementation("com.google.code.gson:gson:2.10.1")
-    implementation("com.russhwolf:multiplatform-settings-serialization:$multiplatformSettings")
-    implementation("com.russhwolf:multiplatform-settings-coroutines:$multiplatformSettings")
-    implementation("com.github.Dansoftowner:jSystemThemeDetector:3.6")
-    implementation("com.google.devtools.ksp:symbol-processing-api:1.9.21-1.0.15")
+
+    implementation(libs.multisettings.noarg)
+    implementation(libs.multisettings.serialization)
+    implementation(libs.multisettings.coroutines)
+    implementation(libs.gson)
+    implementation(libs.jsystemThemeDetector)
+    implementation(libs.ksp)
     // Required
-    implementation("cafe.adriel.lyricist:lyricist:$lyricist")
+    implementation(libs.lyricist.lib)
     // If you want to use @LyricistStrings to generate code for you
-    ksp("cafe.adriel.lyricist:lyricist-processor:$lyricist")
+    ksp(libs.lyricist.processor)
+
 }
 
 compose.desktop {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,12 +4,6 @@ pluginManagement {
         mavenCentral()
         maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
-
-    plugins {
-        kotlin("jvm").version(extra["kotlin.version"] as String)
-        id("org.jetbrains.compose").version(extra["compose.version"] as String)
-        id("com.google.devtools.ksp").version(extra["ksp.version"] as String)
-    }
 }
 
 rootProject.name = "ApkSigner"
