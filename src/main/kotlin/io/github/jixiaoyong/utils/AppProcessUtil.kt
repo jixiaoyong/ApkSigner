b5c8d45e747318a71f29f0ce5245e471af319b18
JI,XIAOYONG <jixiaoyong1995@gmail.com>
2024-05-11 15:35:57 +0800

    feat: 检测APP是否重复运行时名称区分大小写

--- src/main/kotlin/io/github/jixiaoyong/utils/AppProcessUtil.kt
+++ src/main/kotlin/io/github/jixiaoyong/utils/AppProcessUtil.kt
@@ -16,8 +16,11 @@ object AppProcessUtil {
     const val DEFAULT_PROCESS_COUNT = 1
     const val DEFAULT_PROCESS_COUNT_WIN = 2
 
+    /**
+     * 检查指定名称的app是否已在运行
+     * @param appName 当前运行的应用名称，区分大小写（可能和其他同名APP被误认为是同一个）
+     */
     fun isDualAppRunning(appName: String): Boolean {
-        val appName = appName.toLowerCase()
         val osName = System.getProperty("os.name").toLowerCase()
         var processCount = DEFAULT_PROCESS_COUNT
         val command = when {
@@ -34,7 +37,7 @@ object AppProcessUtil {
         var line: String?
         var runningCount = 0
         while (reader.readLine().also { line = it } != null) {
-            if (line!!.toLowerCase().contains(appName)) {
+            if (line!!.contains(appName)) {
                 runningCount++
             }
             if (runningCount > processCount) {
